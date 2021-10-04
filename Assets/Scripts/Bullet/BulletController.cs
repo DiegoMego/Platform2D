7a2a978c7a228940f638195748ff87b33fd6827b
yadiluy <yadiraluyh@gmail.com>
2021-10-04 01:29:13 -0500

    feat: added collider bullet

--- Assets/Scripts/Bullet/BulletController.cs
+++ Assets/Scripts/Bullet/BulletController.cs
@@ -9,6 +9,8 @@ public class BulletController : MonoBehaviour
     private float time = 0f;
     private Vector3 direction = Vector3.right;
 
+    public GameObject powerbar;
+
     private void Start()
     {
         if (transform.rotation.eulerAngles.z == 180f)
@@ -25,5 +27,15 @@ public class BulletController : MonoBehaviour
         }
         time += Time.deltaTime;
         transform.position += direction * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Enemy"))
+        {
+            //aumentar la barra de poder
+
+            Destroy(transform.gameObject);
+        }
     }
 }
