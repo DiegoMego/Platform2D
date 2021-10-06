e5b870e0b42800f6b60e3ea0890dff4552cc1e52
Diego Mego <DM2593@gmail.com>
2021-10-06 05:46:30 +0000

    feat: demon now follows player properly.

--- Assets/Scripts/Demon/DemonController.cs
+++ Assets/Scripts/Demon/DemonController.cs
@@ -4,28 +4,47 @@ using UnityEngine;
 
 public class DemonController : MonoBehaviour
 {
-    private float speed = 7f;
+    private float speed = 3f;
     private float minRangeToMove = 10f;
+    private Vector3 direcion = Vector3.left;
     public bool Running { get; set; }
-    private CapsuleCollider2D collider;
 
     private void Awake()
     {
         Running = false;
-        collider = GetComponent<CapsuleCollider2D>();
     }
 
     private void Update()
     {
         Vector3 heroPos = GameManager.Instance.hero.transform.position;
         Vector3 demonPos = transform.position;
+        Debug.Log($"Distancia: {Vector3.Distance(demonPos, heroPos)}");
         if (Vector3.Distance(demonPos, heroPos) <= minRangeToMove)
         {
             this.Running = true;
         }
         if (this.Running)
         {
-            transform.position += Vector3.MoveTowards(demonPos, heroPos, speed * Time.deltaTime);
+            if (demonPos.x >= heroPos.x)
+            {
+                direcion = Vector3.left;
+                transform.localScale = new Vector3(
+                    1f,
+                    1f,
+                    1f
+                );
+            }
+            else
+            {
+                direcion = Vector3.right;
+                transform.localScale = new Vector3(
+                    -1f,
+                    1f,
+                    1f
+                );
+            }
+            //transform.position += direcion * speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, heroPos, speed * Time.deltaTime);
         }
     }
 }
