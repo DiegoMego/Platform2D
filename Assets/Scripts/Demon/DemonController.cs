faf8c5cfeb133514d39c7b6a969f89691927586c
Diego Mego <DM2593@gmail.com>
2021-10-06 21:58:56 +0000

    feat: enhanced boss ai for movement and implemented its fire attack.

--- Assets/Scripts/Demon/DemonController.cs
+++ Assets/Scripts/Demon/DemonController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,42 +6,62 @@ using UnityEngine;
 public class DemonController : MonoBehaviour
 {
     private float speed = 3f;
-    private float minRangeToMove = 10f;
-    private float minDistanceFromFloor = 5.6f;
+    private float minRangeToMove = 50f;
+    private float minDistanceFromFloor = 6.81f;
+    private float minRangeToAttack = 10f;
+
+    private Animator animator;
+    private GameObject fireBreath;
+    private Transform firePoint;
+    private float timeBetweenAttacks = 5f;
+    private float lastAttackTime = 0f;
     public bool Running { get; set; }
 
     private void Awake()
     {
         Running = false;
+        firePoint = transform.Find("FirePoint");
+    }
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         Vector3 heroPos = GameManager.Instance.hero.transform.position;
         Vector3 demonPos = transform.position;
-        if (Vector3.Distance(demonPos, heroPos) <= minRangeToMove)
+        float distance = Vector3.Distance(demonPos, heroPos);
+        float gameTime = Time.time;
+        if (distance <= minRangeToMove && distance > minRangeToAttack)
         {
             this.Running = true;
         }
+        if (demonPos.x >= heroPos.x)
+        {
+            transform.localScale = new Vector3(
+                1f,
+                1f,
+                1f
+            );
+        }
+        else
+        {
+            transform.localScale = new Vector3(
+                -1f,
+                1f,
+                1f
+            );
+        }
         if (this.Running)
         {
-            if (demonPos.x >= heroPos.x)
-            {
-                transform.localScale = new Vector3(
-                    1f,
-                    1f,
-                    1f
-                );
-            }
-            else
-            {
-                transform.localScale = new Vector3(
-                    -1f,
-                    1f,
-                    1f
-                );
-            }
             transform.position = Vector3.MoveTowards(transform.position, heroPos, speed * Time.deltaTime);
+        }
+        if (distance <= minRangeToAttack && fireBreath == null && gameTime - lastAttackTime > timeBetweenAttacks)
+        {
+            Fire();
+            lastAttackTime = gameTime;
         }
         if (transform.position.y <= minDistanceFromFloor)
         {
@@ -49,6 +70,26 @@ public class DemonController : MonoBehaviour
                 minDistanceFromFloor,
                 0f
             );
+        }
+    }
+
+    private void Fire()
+    {
+        animator.SetTrigger("Fire");
+        if (transform.localScale.x == -1)
+        {
+            fireBreath = Instantiate(GameManager.Instance.breath, firePoint.position, Quaternion.identity);
+            fireBreath.transform.localScale = new Vector3(
+                -1f,
+                1f,
+                1f
+            );
+            fireBreath.transform.parent = transform;
+        }
+        else
+        {
+            fireBreath = Instantiate(GameManager.Instance.breath, firePoint.position, Quaternion.identity);
+            fireBreath.transform.parent = transform;
         }
     }
 }
