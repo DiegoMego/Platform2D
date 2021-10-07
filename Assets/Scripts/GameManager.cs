13c727af1ecea5320c14b76973e01436f24c8df7
DiegoMego <30615834+DiegoMego@users.noreply.github.com>
2021-10-07 02:57:25 +0000

    Enemy health and animations
    Added normal enemies health and dead animations.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,10 +9,13 @@ public class GameManager : MonoBehaviour
     public GameObject hero;
     public GameObject demon;
     public GameObject breath;
+    public GameObject powerBar;
+    public Slider powerBarSlider { get; set; }
 
 
     private void Awake()
     {
         Instance = this;
+        powerBarSlider = powerBar.GetComponent<Slider>();
     }
 }
--- Assets/Scripts/Ghost/GhostController.cs
+++ Assets/Scripts/Ghost/GhostController.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GhostController : MonoBehaviour
 {
     private float speed = 7f;
     public bool Running { get; set; }
+    private Slider slider;
+    private Animator animator;
 
     private void Awake()
     {
         Running = false;
+        animator = GetComponent<Animator>();
+        slider = transform.Find("EnemyHealth").Find("HealthBar").GetComponent<Slider>();
     }
 
     private void Update()
@@ -17,6 +22,22 @@ public class GhostController : MonoBehaviour
         if (Running)
         {
             transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Bullet"))
+        {
+            int damage = collision.transform.GetComponent<BulletController>().damage;
+            slider.value -= damage;
+
+            if (slider.value <= 0)
+            {
+                // Debe morir
+                animator.SetTrigger("IsDead");
+                Running = false;
+            }
         }
     }
 }
